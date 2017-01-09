43575aff42b65c022f782a2c946964cab24fe0a9
Eric <ericrainvillevt@gmail.com>
2017-01-09 18:30:23 -0500

    created User model w tests

--- db/models/user.js
+++ db/models/user.js
@@ -5,32 +5,47 @@ const Sequelize = require('sequelize')
 const db = require('APP/db')
 
 const User = db.define('users', {
-  name: Sequelize.STRING,  
+  firstName: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    },
+  },
   email: {
     type: Sequelize.STRING,
     validate: {
-			isEmail: true,
-			notEmpty: true,
-		}
+      isEmail: true,
+      notEmpty: true,
+    },
   },
-
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
   // We support oauth, so users may or may not have passwords.
   password_digest: Sequelize.STRING,
-	password: Sequelize.VIRTUAL
+  password: Sequelize.VIRTUAL
 }, {
-	indexes: [{fields: ['email'], unique: true,}],
+  indexes: [{
+    fields: ['email'],
+    unique: true,
+  }],
   hooks: {
     beforeCreate: setEmailAndPassword,
     beforeUpdate: setEmailAndPassword,
   },
   instanceMethods: {
     authenticate(plaintext) {
-      return new Promise((resolve, reject) =>
-        bcrypt.compare(plaintext, this.password_digest,
-          (err, result) =>
-            err ? reject(err) : resolve(result))
-        )
-    }    
+      return new Promise((resolve, reject) => bcrypt.compare(plaintext, this.password_digest,
+        (err, result) => err ? reject(err) : resolve(result))
+      )
+    }
   }
 })
 
@@ -38,12 +53,11 @@ function setEmailAndPassword(user) {
   user.email = user.email && user.email.toLowerCase()
   if (!user.password) return Promise.resolve(user)
 
-  return new Promise((resolve, reject) =>
-	  bcrypt.hash(user.get('password'), 10, (err, hash) => {
-		  if (err) reject(err)
-		  user.set('password_digest', hash)
-      resolve(user)
-	  })
+  return new Promise((resolve, reject) => bcrypt.hash(user.get('password'), 10, (err, hash) => {
+    if (err) reject(err)
+    user.set('password_digest', hash)
+    resolve(user)
+  })
   )
 }
 
--- db/models/user.test.js
+++ db/models/user.test.js
@@ -5,17 +5,75 @@ const User = require('./user')
 const {expect} = require('chai')
 
 describe('User', () => {
-  before('wait for the db', () => db.didSync)
+  before('wait for the db', () => db.didSync);
+  let exampleUser;
+  beforeEach('', () => {
+    exampleUser = {
+      firstName: 'eric',
+      lastName: 'rainville',
+      email: 'e@r.com',
+      password: 'ok'
+    }
+  });
+  afterEach('destroy the created user', () => User.destroy({
+    where: {
+      email: 'e@r.com'
+    }
+  }));
 
   describe('authenticate(plaintext: String) ~> Boolean', () => {
-    it('resolves true if the password matches', () =>
-      User.create({ password: 'ok' })
-        .then(user => user.authenticate('ok'))
-        .then(result => expect(result).to.be.true))
+    it('resolves true if the password matches', () => User.create(exampleUser)
+      .then(user => user.authenticate('ok'))
+      .then(result => expect(result).to.be.true))
 
-    it("resolves false if the password doesn't match", () =>
-      User.create({ password: 'ok' })
-        .then(user => user.authenticate('not ok'))
-        .then(result => expect(result).to.be.false))
-  })
+    it("resolves false if the password doesn't match", () => User.create(exampleUser)
+      .then(user => user.authenticate('not ok'))
+      .then(result => expect(result).to.be.false))
+  });
+
+  describe('isAdmin', () => {
+    it('defaults to false', () => User.create(exampleUser)
+      .then(user => expect(user.isAdmin).to.be.false)
+    )
+  });
+
+
+  describe('validations', () => {
+    it('require email', () => {
+      exampleUser.email = '';
+      const user = User.build(exampleUser)
+      return user.validate()
+        .then(err => {
+          // console.log('AAAAAAAAAAAA', err)
+          expect(err).to.be.an('object');
+          expect(err.errors[0].path).to.equal('email') //TODO: fix the hacky array access
+          expect(err.errors[0].type).to.equal('Validation error')
+        })
+
+    });
+    it('require first name', () => {
+      exampleUser.firstName = '';
+      const user = User.build(exampleUser)
+      return user.validate()
+        .then(err => {
+          // console.log('AAAAAAAAAAAA', err)
+          expect(err).to.be.an('object');
+          expect(err.errors[0].path).to.equal('firstName') //TODO: fix the hacky array access
+          expect(err.errors[0].type).to.equal('Validation error')
+        })
+
+    });
+    it('require last name', () => {
+      exampleUser.lastName = '';
+      const user = User.build(exampleUser)
+      return user.validate()
+        .then(err => {
+          // console.log('AAAAAAAAAAAA', err)
+          expect(err).to.be.an('object');
+          expect(err.errors[0].path).to.equal('lastName') //TODO: fix the hacky array access
+          expect(err.errors[0].type).to.equal('Validation error')
+        })
+
+    });
+  });
 })
