fa1675e0ef7d8225289a9564dd67fe31ecd63cb3
Ian Munro <ianmunrobot@gmail.com>
2017-01-10 11:56:28 -0500

    basic model types definition

---
+++ db/models/order.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const db = require('APP/db')
+
+const Order = db.define('orders', {
+  user: {
+    type: Sequelize.STRING
+  },
+  products: {
+    type: Sequelize.ARRAY(Sequelize.JSON)
+  },
+  status: {
+    type: Sequelize.ENUM('in cart', 'placed', 'shipped', 'delivered', 'returned')
+  },
+  tax: {
+    type: Sequelize.DECIMAL(10, 2)
+  },
+  shippingCost: {
+    type: Sequelize.DECIMAL(10, 2)
+  },
+  shippingType: {
+    type: Sequelize.ENUM('')
+  }
+
+})
+
+// associate with a shipping and building address from DB
+Order.belongsTo('address', {as: 'shippingAddress'})
+Order.belongsTo('address', {as: 'billingAddress'})
+
+module.exports = Order
---
+++ db/models/order.test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const db = require('APP/db')
+const Order = require('./order')
+const {expect} = require('chai')
+
+describe('Order', () => {
+  before('wait for the db', () => db.didSync)
+
+  describe('validations', () => {
+
+    it('', () => {
+
+    })
+
+
+  })
+
+})
