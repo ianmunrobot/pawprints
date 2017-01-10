87b76b637457cceeb0b065a44a9e6c995fe8d985
Ian Munro <ianmunrobot@gmail.com>
2017-01-10 18:10:30 -0500

    working on routes

--- server/products.js
+++ server/products.js
@@ -1,16 +1,11 @@
 'use strict'
 
 const db = require('APP/db')
-const Product = db.model('products')
+const Product = db.model('product')
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   Product.findAll()
-  .then(result => {
-    console.log(result)
-    console.log('~~~~~~~~~~~');
-    return result
-  })
   .then(res.send.bind(res))
   .catch(next)
 })
--- server/products.test.js
+++ server/products.test.js
@@ -5,13 +5,63 @@ const Product = require('APP/db/models/product')
 const app = require('./start')
 
 describe('/api/products', () => {
+  before('wait for the db', () => db.didSync)
+
+  var option1, option2, option3
+  before(() => {
+    return Product.destroy({
+        truncate: true,
+        cascade: true,
+      })
+    .then(() => {
+      option1 = Product.build({
+        title: 'product routes test 1',
+        description: 'a very cool product',
+        price: 2.50,
+        inventory: 3,
+      })
+      option2 = Product.build({
+        title: 'product routes test 2',
+        description: 'a very cool product',
+        price: 2.50,
+        inventory: 4,
+      })
+      option3 = Product.build({
+        title: 'product routes test 3',
+        description: 'a very cool product',
+        price: 2.50,
+        inventory: 1,
+      })
+
+      return Promise.all([option1.save(), option2.save(), option3.save()])
+      .then(values => {
+        return values[0].setOptions([option2, option3])
+      })
+    })
+  })
+
+  after(() => {
+    return Product.truncate(
+      {
+        truncate: true,
+        cascade: true,
+      })
+  })
+
+
   describe('when not logged in', () => {
-    it('GET returns all products', () => {
+    it('GET / returns all products', () => {
       request(app)
         .get(`/api/products`)
-        .then(result => {
-          console.log(result)
+        .then(response => {
+          console.log(response);
+          expect(response.body.length).to.equal(3)
         })
+    })
+
+    it('GET /:id returns a single product', () => {
+      request(app)
+        .get(`api/products/`)
     })
 
   })
