18cde6be41ddcc67c00394a45cb031382e27aab4
Ian Munro <ianmunrobot@gmail.com>
2017-01-10 18:27:42 -0500

    catching up to master

--- db/models/product.js
+++ db/models/product.js
@@ -1,52 +1,54 @@
 const Sequelize = require('sequelize')
 const db = require('APP/db')
-const Category = require(('./categories'))
 
 const Product = db.define('products', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  urlTitle: {
-    type: Sequelize.STRING
-  },
-  imgUrl: {
-    type: Sequelize.STRING,
-    defaultValue: 'http://placekitten.com/g/300/200/'
-  },
-  price: {
-    type: Sequelize.FLOAT,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
-  },
-  size: {
-    type: Sequelize.STRING,
-  },
-  tags: {
-    type: Sequelize.ARRAY(Sequelize.STRING)
-  },
-  inventory: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-
-},
-{
-  hooks: {
-    beforeValidate: function (product) {
-      if (product.title) {
-        product.urlTitle = product.title.replace(/\s+/g, '_').replace(/\W/g, '');
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
       }
+    },
+    urlTitle: {
+      type: Sequelize.STRING
+    },
+    imgUrl: {
+      type: Sequelize.STRING,
+      defaultValue: 'http://placekitten.com/g/300/200/'
+    },
+    price: {
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    size: {
+      type: Sequelize.STRING,
+    },
+    tags: {
+      type: Sequelize.ARRAY(Sequelize.STRING)
+    },
+    inventory: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
 
-
-    }
   },
+  {
+    hooks: {
+      beforeValidate: function (product) {
+        if (product.title) {
+          product.urlTitle = product.title.replace(/\s+/g, '_').replace(/\W/g, '');
+        }
+
+
+      }
+    },
 })
 
 // associate multiple rows as size options for each product
