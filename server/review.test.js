2124a831caacff1bed59cbfff345f3f8b23de810
Jimmy <jdicolandrea@gmail.com>
2017-01-11 14:53:51 -0500

    wrote review get requests

--- server/review.test.js
+++ server/review.test.js
@@ -5,37 +5,51 @@ const Review = require('APP/db/models/review');
 const Product = require('APP/db/models/product');
 const app = require('./start')
 
+import chai from 'chai'
+import supertest from 'supertest-as-promised'
+
+// describe('Review routes tests', () => {
+//   beforeEach('Synchronize and clear database', () => db.sync({force: true}));
+//
+//   after('Synchronize and clear database', () => db.sync({force: true}));
+//
+//   let agent;
+//   beforeEach('Set up agent for testing', () => {
+//       agent = supertest(app);
+//   });
+//
+//   let dogPhoto = {
+//     id = 99
+//     title: 'clifford',
+//     price: 3.50,
+//     description: 'big and red'
+//     inventory: 1,
+//   };
+//
+//
+//
+// })
+
 describe('/api/products/:productId/reviews', () => {
   describe('for a product', () => {
-    it('GETs all reviews', () => {
-      request(app)
-        .get('/api/products/1/reviews')
-        .expect(200)
-    });
-    it('POST creates a new review', () => {
-      request(app)
-        .post('/api/products/1/reviews')
-        .expect(201)
-    });
-    it('POST redirects to the review that was just made', () => {
-      request(app)
-        .post('/api/products/1/reviews/1')
-        .expect(201)
-    })
+    it('GET all reviews', () => request(app)
+      .get('/api/products/1/reviews')
+      .expect(200)
+    );
   })
-});
-
-describe('/api/products/:productId/reviews/:reviewId', () => {
-  describe('for a specific product review', () => {
-    it('PUT updates a review in the database', () => {
-      request(app)
-        .post('/api/products/1/reviews/1')
-        .expect(202)
-    });
-    it('PUT redirects to the review that was just updated', () => {
-      request(app)
-        .post('/api/products/1/reviews/1')
-        .expect(202)
-    })
+  describe('for a user', () => {
+    it ('GET all reviews', () => request(app)
+      .get('/api/users/1/reviews')
+      .expect(200)
+    )
   })
-});
+    // it('POST creates a new review', () => request(app)
+    //   .post('/api/products/1/reviews')
+    //   .expect(201)
+    // );
+    // it('POST redirects to the review that was just made', () => request(app)
+    //   .post('/api/products/1/reviews/1')
+    //   .expect(201)
+    // );
+  // })
+})
