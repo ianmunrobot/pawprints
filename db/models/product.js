b0a8783f50ba4728971ee15d74c597399b3a79cf
Ian Munro <ianmunrobot@gmail.com>
2017-01-11 14:12:53 -0500

    up-to-date with master

--- db/models/product.js
+++ db/models/product.js
@@ -1,58 +1,51 @@
 const Sequelize = require('sequelize')
 const db = require('APP/db')
 
-const Product = db.define('products', {
-    title: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true
-      }
-    },
-    urlTitle: {
-      type: Sequelize.STRING
-    },
-    imgUrl: {
-      type: Sequelize.STRING,
-      defaultValue: 'http://placekitten.com/g/300/200/'
-    },
-    price: {
-      type: Sequelize.FLOAT,
-      allowNull: false,
-    },
-    description: {
-      type: Sequelize.TEXT,
-      allowNull: false,
-      validate: {
-        notEmpty: true
-      }
-    },
-    size: {
-      type: Sequelize.STRING,
-    },
-    tags: {
-      type: Sequelize.ARRAY(Sequelize.STRING)
-    },
-    inventory: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    },
-
+const Product = db.define('product', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
-  {
-    hooks: {
-      beforeValidate: function (product) {
-        if (product.title) {
-          product.urlTitle = product.title.replace(/\s+/g, '_').replace(/\W/g, '');
-        }
-
-
+  urlTitle: {
+    type: Sequelize.STRING
+  },
+  imgUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'http://placekitten.com/g/300/200/'
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  size: {
+    type: Sequelize.STRING,
+  },
+  tags: {
+    type: Sequelize.ARRAY(Sequelize.STRING)
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+},
+{
+  hooks: {
+    beforeValidate: function (product) {
+      if (product.title) {
+        product.urlTitle = product.title.replace(/\s+/g, '_').replace(/\W/g, '');
       }
-    },
+    }
+  },
 })
 
-// associate multiple rows as size options for each product
-Product.belongsToMany(Product, {through: 'options', as: 'options'})
-
 module.exports = Product
-
