9a9afdc513e3c2e63be56d9377fe14ef2456532e
Jimmy <jdicolandrea@gmail.com>
2017-01-10 20:33:30 -0500

    make bare bones for review routes

--- server/review.test.js
+++ server/review.test.js
@@ -3,18 +3,39 @@ const {expect} = require('chai');
 const db = require('APP/db');
 const Review = require('APP/db/models/review');
 const Product = require('APP/db/models/product');
+const app = require('./start')
 
 describe('/api/products/:productId/reviews', () => {
   describe('for a product', () => {
-    it('GETs all reviews', () => {});
-    it('POST creates a new review', () => {});
-    it('POST redirects to the review that was just made', () => {})
+    it('GETs all reviews', () => {
+      request(app)
+        .get('/api/products/1/reviews')
+        .expect(200)
+    });
+    it('POST creates a new review', () => {
+      request(app)
+        .post('/api/products/1/reviews')
+        .expect(201)
+    });
+    it('POST redirects to the review that was just made', () => {
+      request(app)
+        .post('/api/products/1/reviews/1')
+        .expect(201)
+    })
   })
 });
 
 describe('/api/products/:productId/reviews/:reviewId', () => {
   describe('for a specific product review', () => {
-    it('PUT updates a review in the database', () => {});
-    it('PUT redirects to the review that was just updated')
+    it('PUT updates a review in the database', () => {
+      request(app)
+        .post('/api/products/1/reviews/1')
+        .expect(202)
+    });
+    it('PUT redirects to the review that was just updated', () => {
+      request(app)
+        .post('/api/products/1/reviews/1')
+        .expect(202)
+    })
   })
 });
