50c506eded1b366243f990e1247dc51cc489e3bd
Charity Corcino <cecicorcino@gmail.com>
2017-01-11 16:13:04 -0500

    Index Html Updated

--- app/components/App.js
+++ app/components/App.js
@@ -1,2 +1,22 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+
+export default connect(null)(({children}) =>
+	<div class="top_bg" id="home">
+	<div class="container">
+		<div class="header_top">
+			<div class="top_right">
+				<ul>
+					<li><a href="#">help</a></li>
+					<li><a href="contact.html">Contact</a></li>
+					<li><a href="#">Delivery information</a></li>
+				</ul>
+			</div>
+			<div class="top_left">
+				<h6><span></span> Call us : 032 2352 782</h6>
+			</div>
+				<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+)
