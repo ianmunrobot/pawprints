f1900dac8e3ba9d49ebbc9411d86ee5acdb77570
Ian Munro <ianmunrobot@gmail.com>
2017-01-10 19:49:33 -0500

    updating admin tests

--- server/products.js
+++ server/products.js
@@ -1,13 +1,40 @@
 'use strict'
 
 const db = require('APP/db')
-const Product = db.model('product')
+const Product = db.model('products')
 const router = require('express').Router()
 
+// get all products
 router.get('/', (req, res, next) => {
   Product.findAll()
   .then(res.send.bind(res))
   .catch(next)
 })
+
+// get product by id
+router.get('/:id', (req, res, next) => {
+  Product.findById(req.params.id)
+  .then(res.send.bind(res))
+  .catch(next)
+})
+
+// update product
+router.put('/:id', (req, res, next) => {
+  if (req.user.isAdmin) {
+    Product.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+      returning: true,
+    })
+    .then(response => response[1])
+    .then(res.send.bind(res))
+    .catch(next)
+  } else {
+    res.sendStatus(403)
+  }
+})
+
+
 
 module.exports = router
--- server/products.test.js
+++ server/products.test.js
@@ -1,8 +1,13 @@
 const request = require('supertest-as-promised')
-const {expect} = require('chai')
+const chai = require('chai')
 const db = require('APP/db')
 const Product = require('APP/db/models/product')
 const app = require('./start')
+const chaiProperties = require('chai-properties');
+const chaiThings = require('chai-things');
+chai.use(chaiProperties);
+chai.use(chaiThings);
+const expect = chai.expect;
 
 describe('/api/products', () => {
   before('wait for the db', () => db.didSync)
@@ -52,23 +57,35 @@ describe('/api/products', () => {
 
 
   describe('when not logged in', () => {
-    it('GET / returns all products', () => {
+    it('GET / returns all products', () =>
       request(app)
         .get(`/api/products`)
         .then(response => {
-          console.log(response);
           expect(response.body.length).to.equal(3)
-        })
-    })
+          expect(response.body).to.contain.a.thing.with('inventory', 3)
+          expect(response.body).to.contain.a.thing.with('inventory', 4)
+        }))
 
-    it('GET /:id returns a single product', () => {
+
+    it('GET /:id returns a single product', () =>
       request(app)
-        .get(`api/products/`)
-    })
+        .get(`/api/products/1`)
+        .then(response => {
+          expect(response.body).to.be.an('object')
+          expect(response.body.id).to.equal(1)
+          expect(response.body.description).to.be.equal('a very cool product')
+        }))
 
   })
 
   describe('when admin', () => {
-
+    it('PUT /:id alters a product', (req, res, next) => {
+      return request(app)
+        .put(`/api/products/1`)
+        .then(response => {
+          console.log(response)
+          expect(response.body).to.be.an('object')
+        })
+    })
   })
 })
