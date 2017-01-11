1f0e4031bbaeaa89bbb977e5888e288102e2d940
Ian Munro <ianmunrobot@gmail.com>
2017-01-11 11:46:44 -0500

    added mustBeAdmin filter

--- server/auth.filters.js
+++ server/auth.filters.js
@@ -5,6 +5,13 @@ const mustBeLoggedIn = (req, res, next) => {
   next()
 }
 
+const mustBeAdmin = (req, res, next) => {
+  if (!(req.user && req.user.isAdmin)) {
+    return res.status(401).send('You must be an admin to complete that action')
+  }
+  next()
+}
+
 const selfOnly = action => (req, res, next) => {
   if (req.params.id !== req.user.id) {
     return res.status(403).send(`You can only ${action} yourself.`)
@@ -16,4 +23,4 @@ const forbidden = message => (req, res, next) => {
   res.status(403).send(message)
 }
 
-module.exports = {mustBeLoggedIn, selfOnly, forbidden,}
+module.exports = {mustBeLoggedIn, selfOnly, forbidden, mustBeAdmin,}
