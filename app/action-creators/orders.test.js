58107bf5cf07cc1a9954f5e48a82f7a3a7e684a9
Ian Munro <ianmunrobot@gmail.com>
2017-01-12 11:49:08 -0500

    closes create orders action creators #76

---
+++ app/action-creators/orders.test.js
@@ -0,0 +1,35 @@
+import { expect } from 'chai'
+
+import { receiveOrders, receiveOrder } from './orders'
+
+describe('Orders actions', () => {
+  const testOrders = [
+    {
+      status: 'shipped',
+      shippingCost: 10.50,
+      isGift: true,
+      tax: 7.56,
+    },
+    {
+      status: 'delivered',
+      shippingCost: 30.54,
+      isGift: false,
+      tax: 10.43,
+    },
+  ]
+
+  it('receives a single order', () => {
+    expect(receiveOrder(testOrders[0])).to.be.deep.equal({
+      type: 'RECEIVE_ORDER',
+      order: testOrders[0],
+    })
+  })
+
+  it('receives all orders', () => {
+    expect(receiveOrders(testOrders)).to.be.deep.equal({
+      type: 'RECEIVE_ORDERS',
+      orders: testOrders,
+    })
+  })
+
+})
