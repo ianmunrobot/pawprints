6de9a5bb0aab3e767b67075b73c4e50bd06cc415
Charity Corcino <cecicorcino@gmail.com>
2017-01-12 12:44:16 -0500

    Add-HomePage-Component-#95

---
+++ app/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Display from './HomePageDisplay';
+import Footer from './Footer';
+
+export default function homePage() {
+	return (
+
+			<div>
+			<Display />
+			<Footer />
+			</div>
+		)
+}
