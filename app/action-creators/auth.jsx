5302b44f65b7f349b11a33f3f4dfc07e98fd76db
Ian Munro <ianmunrobot@gmail.com>
2017-01-12 10:55:58 -0500

    refactored auth action creators

---
+++ app/action-creators/auth.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+import { AUTHENTICATED } from 'APP/app/constants'
+
+export const authenticated = user => ({
+  type: AUTHENTICATED,
+  user,
+})
+
+export const login = (username, password) =>
+  dispatch =>
+    axios.post('/api/auth/local/login',
+      {username, password})
+      .then(() => dispatch(whoami()))
+      .catch(() => dispatch(whoami()))
+
+export const logout = () =>
+  dispatch =>
+    axios.post('/api/auth/logout')
+      .then(() => dispatch(whoami()))
+      .catch(() => dispatch(whoami()))
+
+export const whoami = () =>
+  dispatch =>
+    axios.get('/api/auth/whoami')
+      .then(response => {
+        const user = response.data
+        dispatch(authenticated(user))
+      })
+      .catch(failed => dispatch(authenticated(null)))
