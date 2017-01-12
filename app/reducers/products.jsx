bfe5e6bd8b8a73dbc96cae0031feadb188a0ef4c
Ian Munro <ianmunrobot@gmail.com>
2017-01-12 18:51:37 -0500

    closes products-reducer-#70

--- app/reducers/products.jsx
+++ app/reducers/products.jsx
@@ -1,8 +1,29 @@
-const productsReducer = (state, action) => {
-  switch(action.type) {
+import {
+  RECEIVE_PRODUCT,
+  RECEIVE_PRODUCTS,
+} from 'APP/app/constants'
 
+const initialState = {
+  allProducts: [],
+  selectedProduct: {},
+}
+
+const productsReducer = (state = initialState, action) => {
+  const newState = Object.assign({}, state)
+
+  switch(action.type) {
+    case RECEIVE_PRODUCT:
+      newState.selectedProduct = action.product
+      break;
+
+    case RECEIVE_PRODUCTS:
+      newState.allProducts = action.products
+      break;
+
+    default:
+      return state
   }
-  return state
+  return newState
 }
 
 export default productsReducer
