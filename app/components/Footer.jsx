c0818c0d8fc9c75f0c044c9146d50c618a233aa6
Charity Corcino <cecicorcino@gmail.com>
2017-01-12 21:43:43 -0500

    closes-update-footer-#123

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -12,11 +12,7 @@ export default function footer() {
 				<div className="col-md-2 footer-left">
 					<h3>About Us</h3>
 					<ul>
-						<li><a href="#">Who We Are</a></li>
-						<li><a href="contact.html">Contact Us</a></li>
-						<li><a href="#">Our Sites</a></li>
-						<li><a href="#">In The News</a></li>
-						<li><a href="#">Carrers</a></li>					 
+						<li><a href="contact.html">Contact Us</a></li>				 
 					</ul>
 				</div>
 				<div className="col-md-2 footer-left">
@@ -27,36 +23,6 @@ export default function footer() {
 						<li><a href="contact.html">Addresses</a></li>
 						<li><a href="#">Discount</a></li>
 						<li><a href="#">Track your order</a></li>					 					 
-					</ul>
-				</div>
-				<div className="col-md-2 footer-left">
-					<h3>Shopping</h3>
-					<ul>
-						<li><a href="#">Accesories</a></li>
-						<li><a href="#">Books</a></li>
-						<li><a href="#">Cloths</a></li>
-						<li><a href="#">Bags</a></li>
-						<li><a href="#">Shoes</a></li>					 
-					</ul>
-				</div>
-				<div className="col-md-2 footer-left ">
-					<h3>Categories</h3>
-					<ul>
-						<li><a href="#">Sports Shoes</a></li>
-						<li><a href="#">Casual Shorts</a></li>
-						<li><a href="#">Formal Shoes</a></li>
-						<li><a href="#">Party Wear</a></li>
-						<li><a href="#">Ethnic Wear</a></li>				 
-					</ul>
-				</div>
-				<div className="col-md-2 footer-left lost">
-					<h3>Life Style</h3>
-					<ul>
-						<li><a href="#">Spa</a></li>
-						<li><a href="#">Beauty</a></li>
-						<li><a href="#">Travel</a></li>
-						<li><a href="#">Food</a></li>
-						<li><a href="#">Trends</a></li>					 
 					</ul>
 				</div>
 				<div className="clearfix"> </div>
