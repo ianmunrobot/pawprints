2350c40cec606dd7d025d3d9b9f24ca03b1473b8
Jimmy <jdicolandrea@gmail.com>
2017-01-13 14:36:07 -0500

    create basic products container and component

---
+++ app/components/products/AllProducts.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } form 'react-router';
+
+export default class AllProducts extends Component {
+  componentDidMount () {
+
+  }
+
+  render () {
+    return (
+      <div>
+        {
+          this.props.products.map(product => {
+            return (
+              <ul key={product.id}>
+                <li>
+                  <Link to={`/products/${product.id}`}>{product.name}</Link>
+                </li>
+              </ul>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
