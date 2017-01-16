10eb1fdeb6424bcee0318ad3dd6b0571916fe3e7
Charity Corcino <cecicorcino@gmail.com>
2017-01-16 10:42:48 -0500

    restart comp

---
+++ app/components/HomePageDisplayTests.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import chai, {expect} from 'chai'
+chai.use(require('chai-enzyme')())
+import {shallow} from 'enzyme'
+import {spy} from 'sinon'
+chai.use(require('sinon-chai'))
+
+import Display from './HomePageDisplay'
+
+
+describe('<Display/>', () => {
+  let root
+  beforeEach('render the root', () =>
+    root = shallow(<Display />)
+  )
+
+   it('shows a title network image', () => {
+   	let title = root.find('.title');
+    expect(title).to.equal('PawPrints')
+  })
+})
