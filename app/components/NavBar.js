db40a5451b2330fdf22f94c80ce72e7ee3fd0577
Charity Corcino <cecicorcino@gmail.com>
2017-01-16 11:36:47 -0500

    closes #142

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -5,10 +5,11 @@ export default function navBar() {
 	return (
 	<ul className="megamenu skyblue">
 				<li className="active grid"><Link to="/" className="color1">Home</Link></li>
-				<li className="grid"><a className="color2" href="#">Categories</a>
-					</li>
+				<li className="grid"><a className="color2" href="#">Puppies</a></li>
+				<li className="grid"><a className="color2" href="#">Kittens</a></li>
+				<li className="grid"><a className="color2" href="#">Other Pets</a></li>
 				<li><Link to="/signup">Your Account</Link></li>
-				<li><a href="#">Track your order</a></li>				
-			 </ul> 
+				<li><a href="#">Track your order</a></li>
+	</ul> 
 		   );
 }
