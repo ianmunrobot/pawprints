808da1ae9cd1c09faddc57583b01e31291b37887
Charity Corcino <cecicorcino@gmail.com>
2017-01-16 18:26:35 -0500

    closes #145

--- app/components/UserPanel.jsx
+++ app/components/UserPanel.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
 export function UserPanel({user}) {
-	return (
+
+
+	if (!user) return (<div></div>)
+		else return (
 		<div>
+		 <div className="account account-left">
+       	 <div className="container">
+          <div className="account-bottom">
+			<div className="col-md-6 account-left">
 			<form>
                 <div className="account-top heading">
                   <h3>My Information</h3>
@@ -16,10 +23,49 @@ export function UserPanel({user}) {
                 <div className="address">
                   <span>Email Address: {user.email}</span>
                 </div>
+              </form>
+              </div>
+              <div className="col-md-6 account-left">
+              <form id="userInfo" onSubmit={ function(evt){
+					evt.preventDefault()
+					let firstName= evt.target.firstName.value;
+					let lastName= evt.target.lastName.value;
+					let email= evt.target.email.value;
+					let password= evt.target.password.value;
+					console.log('SUBMIT BUTTON PRESSED')
+					signUp(firstName, lastName, email , password)
+							document.getElementById("userInfo").reset()}}>
+                <div className="account-top heading">
+                  <h3>Update Information</h3>
+                </div>
+                <div className="address">
+                  <span>First Name</span>
+                  <input type="text" name="firstName" />
+                </div>
+                <div className="address">
+                  <span>Last Name</span>
+                  <input type="text" name="lastName" />
+                </div>
+                <div className="address">
+                  <span>Email Address</span>
+                  <input type="text" name="email" />
+                </div>
+                <div className="address">
+                  <span>Password</span>
+                  <input type="password" name="password" />
+                </div>
+                <div className="address">
+                  <span>Re-enter Password</span>
+                  <input type="password" />
+                </div>
                 <div className="address new">
-                  <input type="submit" value="submit" />
+                  <input type="submit" value="update" />
                 </div>
               </form>
+              </div> 
+              </div>
+              </div>
+              </div>
 		</div>
 		)
 }
