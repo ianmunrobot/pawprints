e56daec8fb8810f604d29bef00d9c345b6b68f0a
Charity Corcino <cecicorcino@gmail.com>
2017-01-16 15:33:59 -0500

    closes #130

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
+import Login from './Login';
 
-export default function signUp() {
+export function signUp() {
 	return (
 		<div>
 			<div className="account account-left" >
 	  <div className="container" >
 	       <div className="account-bottom">
 				<div className="col-md-6 account-left">
-					<form>
+					<form onSubmit={function(evt){
+						evt.preventDefault()
+						let email= evt.target.email.value; 
+						console.log(email)
+						let password= evt.target.password.value;
+						console.log(password)
+						login(email , password)}}>
 					<div className="account-top heading" >
 						<h3>NEW CUSTOMERS</h3>
 					</div>
-					<div className="address">
+					<div className="address" >
 						<span>First Name</span>
-						<input type="text" />
+						<input type="text" name="firstName"/>
 					</div>
-					<div className="address">
+					<div className="address" >
 						<span>Last Name</span>
-						<input type="text" />
+						<input type="text" name="lastName"/>
 					</div>
-					<div className="address">
+					<div className="address" >
 						<span>Email Address</span>
-						<input type="text" />
+						<input type="text" name="email"/>
 					</div>
-					<div className="address">
+					<div className="address" >
 						<span>Password</span>
-						<input type="password" />
+						<input type="password" name="password"/>
 					</div>
 					<div className="address">
 						<span>Re-enter Password</span>
@@ -43,3 +50,13 @@ export default function signUp() {
 		</div>
 		)
 }
+
+//---------------Login Container --------------------//
+import {login} from 'APP/app/action-creators/auth'
+import {connect} from 'react-redux'
+
+export default connect (
+  state => ({}),
+  {login})(signUp)
+
+
