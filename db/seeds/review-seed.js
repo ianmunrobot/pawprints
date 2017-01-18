6675dfe0182e0ab1e870af729e637895b5b623e4
Charity Corcino <cecicorcino@gmail.com>
2017-01-18 09:26:34 -0500

    closes #253

--- db/seeds/review-seed.js
+++ db/seeds/review-seed.js
@@ -28,6 +28,30 @@ const seedReviews = () => db.Promise.map([
     title: `'I've seen cuter`,
     message: `dogs`,
     rating: 3
+  },{
+    title: `Cute! Cute! Cute!`,
+    message: `And cuter!`,
+    rating: 5
+  },{
+    title: `You call this a canvas?`,
+    message: `Spend a few more dollars on your material. I could have made this with my 5 year old!`,
+    rating: 1
+  },{
+    title: `OMG!!!`,
+    message: `OMG, these animals are soooooooooo totally adorbz!! LOL Totally luvd it!!!`,
+    rating: 5
+  }, {
+    title: `Ugh`,
+    message: `These are some fugly canvases`,
+    rating: 2
+  }, {
+    title: `Ehh, it's okay...`,
+    message: `You get what you pay for.`,
+    rating: 3
+  }, {
+    title: `My daughter loves it!`,
+    message: `I bought some to hang in her room and she loves it. Proud Mama!`,
+    rating: 4
   },
 ], review => {
   return db.model('reviews').create(review)
